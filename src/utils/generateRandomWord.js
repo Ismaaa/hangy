ef8edd346e7186f96403ec0474870e9a550cfc55
Isma <didouhismael@gmail.com>
2020-08-10 23:50:07 +0200

    fix special chars and digits check

--- src/utils/generateRandomWord.js
+++ src/utils/generateRandomWord.js
@@ -1,16 +1,16 @@
 import faker from 'faker';
 
 export default function generateRandomWord() {
-  const randomWord = faker.random.word().toUpperCase().split(' ')[0];
+  let randomWord = faker.random.word().toUpperCase().split(' ')[0];
 
   // digits
   if (/\d/.test(randomWord)) {
-    generateRandomWord();
+    randomWord = generateRandomWord();
   }
 
   // special chars
   if (/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(randomWord)) {
-    generateRandomWord();
+    randomWord = generateRandomWord();
   }
 
   return randomWord.toUpperCase().split(' ')[0];
