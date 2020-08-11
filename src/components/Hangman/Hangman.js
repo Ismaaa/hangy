c658ca25af16441c36e8085af4e68e593bd5ae18
Isma <didouhismael@gmail.com>
2020-08-11 20:42:39 +0200

    hardcoded hangman

--- src/components/Hangman/Hangman.js
+++ src/components/Hangman/Hangman.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const Hangman = () => <div className="Hangman">FIGURE HERE</div>;
+const Hangman = () => (
+  <div className="Hangman">
+    <span className="Hangman__footer" />
+    <span className="Hangman__bar--main" />
+    <span className="Hangman__bar--top" />
+    <span className="Hangman__bar--rope" />
+    <span className="Hangman__person--head" />
+    <span className="Hangman__person--body" />
+    <span className="Hangman__person--leftArm" />
+    <span className="Hangman__person--rightArm" />
+    <span className="Hangman__person--leftLeg" />
+    <span className="Hangman__person--rightLeg" />
+    <span className="Hangman__person--diagonal" />
+  </div>
+);
 
 export default Hangman;
